ba029898da206867bd1034d06ba1136fa9380fc2
Fabian Paul <104452360+fp039@users.noreply.github.com>
2025-06-05 16:58:35 +0200

    fix tech stack icons

--- src/components/TechCard.vue
+++ src/components/TechCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div data-tech-card class="bg-[#2A2A2A] p-8 rounded-2xl h-full flex flex-col">
-    <img :src="icon" :alt="name" class="w-12 h-12 mb-6" />
+    <img :src="iconSrc" :alt="name" class="w-12 h-12 mb-6" />
     <h3 class="text-fluid-lg font-medium text-text-primary mb-2">{{ name }}</h3>
     <span class="text-accent text-fluid-sm mb-4">{{ category }}</span>
     <p class="text-text-secondary/80 text-fluid-sm">{{ description }}</p>
@@ -23,8 +23,15 @@ export default {
       required: true
     },
     icon: {
-      type: String,
+      type: [String, Object],
       required: true
+    }
+  },
+  computed: {
+    iconSrc() {
+      return typeof this.icon === 'object' && this.icon !== null
+        ? this.icon.src
+        : this.icon
     }
   },
   methods: {
